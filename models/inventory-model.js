5db2aa1ce5fbf6285547956c4bf13b69a70e1c7e
nzilla <victorbliss044@gmail.com>
2025-07-14 15:57:08 +0100

    commit

---
+++ models/inventory-model.js
@@ -0,0 +1,11 @@
+const pool = require('../database/index.js'); // Importing the pool from database/index.js
+
+
+/* ***************************
+ *  Get all classification data
+ * ************************** */
+async function getClassifications(){
+  return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
+}
+
+module.exports = {getClassifications}
